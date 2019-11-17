253862953199f66d2d514f659f38b55687d78651
aredra <rnswn48@gmail.com>
2019-11-17 23:58:45 +0900

    add getter, setter

--- src/beginJava/Student.java
+++ src/beginJava/Student.java
@@ -1,11 +1,29 @@
 package beginJava;
 
 public class Student {
-    public int studentID;
-    public String studentName;
-    public String address;
+    private int studentID;
+    private String studentName;
+    private String address;
 
     public void showStudentInfo() {
         System.out.println(studentName + ", " + address);
+    }
+    public void setStudentID(int id) {
+        this.studentID = id;
+    }
+    public int getStudentID() {
+        return this.studentID;
+    }
+    public void setStudentName(String name) {
+        this.studentName = name;
+    }
+    public String getStudentName() {
+        return this.studentName;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return this.address;
     }
 }
--- src/beginJava/StudentTest.java
+++ src/beginJava/StudentTest.java
@@ -3,8 +3,8 @@ package beginJava;
 public class StudentTest {
     public static void main(String[] args) {
         Student student1 = new Student();
-        student1.studentName = "Aredra";
-        student1.address = "Seoul";
+        student1.setStudentName("Aredra");
+        student1.setAddress("Seoul");
 
         student1.showStudentInfo();
     }
