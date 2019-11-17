d8534f059a29693a14d53989ebee87cb57ddc0e8
jy.Seo <dev1.wizdome.co.kr>
2019-11-17 23:34:51 +0900

    add main

---
+++ src/beginJava/Student.java
@@ -0,0 +1,11 @@
+package beginJava;
+
+public class Student {
+    public int studentID;
+    public String studentName;
+    public String address;
+
+    public void showStudentInfo() {
+        System.out.println(studentName + ", " + address);
+    }
+}
---
+++ src/beginJava/StudentTest.java
@@ -0,0 +1,11 @@
+package beginJava;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.studentName = "Aredra";
+        student1.address = "Seoul";
+
+        student1.showStudentInfo();
+    }
+}
