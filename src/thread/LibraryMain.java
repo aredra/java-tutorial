86985a3ebdda377bc28eab4153fd3cd6e739623d
aredra <rnswn48@gmail.com>
2020-02-03 17:14:16 +0900

    add wait/notify sample

---
+++ src/thread/LibraryMain.java
@@ -0,0 +1,63 @@
+package thread;
+
+import java.util.ArrayList;
+
+class FastLibrary {
+    public ArrayList<String> books = new ArrayList<>();
+
+    public FastLibrary() {
+        books.add("A");
+        books.add("B");
+        books.add("C");
+//        books.add("D");
+//        books.add("E");
+//        books.add("F");
+    }
+
+    public synchronized String lendBook() throws InterruptedException{
+        Thread th = Thread.currentThread();
+        while (books.size() == 0) {
+            System.out.println(th.getName() + " is lending");
+            wait();
+        }
+        String title = books.remove(0);
+        System.out.println(th.getName() + " lend : " + title);
+        return title;
+    }
+
+    public synchronized void reurnBook(String title) {
+        Thread th = Thread.currentThread();
+        books.add(title);
+        notifyAll();
+        System.out.println(th.getName() + " return : " + title);
+    }
+}
+
+class Person extends Thread {
+    public void run() {
+        try {
+            String title = LibraryMain.library.lendBook();
+            sleep(2000);
+            LibraryMain.library.reurnBook(title);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class LibraryMain {
+    public static FastLibrary library = new FastLibrary();
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        Person p2 = new Person();
+        Person p3 = new Person();
+        Person p4 = new Person();
+        Person p5 = new Person();
+
+        p1.start();
+        p2.start();
+        p3.start();
+        p4.start();
+        p5.start();
+    }
+}
---
+++ src/thread/SyncTest.java
@@ -0,0 +1,75 @@
+package thread;
+
+class Bank {
+    private int money = 100000;
+
+    public synchronized void saveMoney(int save) {
+        int m = this.getMoney();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        setMoney(m + save);
+    }
+
+    public synchronized void minusMoney(int minus) throws Exception {
+        int m = this.getMoney();
+
+        if (m < minus) {
+            throw new Exception("Balance is lack");
+        }
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        setMoney(m - minus);
+    }
+
+    public int getMoney() {
+        return money;
+    }
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
+
+class T1 extends Thread {
+    public void run() {
+        synchronized (SyncTest.myBank) {
+            System.out.println("start save");
+            SyncTest.myBank.saveMoney(4000);
+            System.out.println(super.getName()+" result = "+SyncTest.myBank.getMoney());
+        }
+    }
+}
+
+class T2 extends Thread {
+    public void run() {
+        System.out.println("start minus");
+        try {
+            SyncTest.myBank.minusMoney(5000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(super.getName()+" result = "+SyncTest.myBank.getMoney());
+    }
+}
+
+public class SyncTest {
+    public static Bank myBank = new Bank();
+    public static void main(String[] args) throws InterruptedException{
+        T1 t1 = new T1();
+        t1.start();
+
+        Thread.sleep(1000);
+
+        T2 t2 = new T2();
+        t2.start();
+    }
+}
