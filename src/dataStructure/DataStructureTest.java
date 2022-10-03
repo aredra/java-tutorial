6eb6dfe4727133b7ea84f986ec71719be8717b4c
aredra <rnswn48@gmail.com>
2022-10-04 00:13:56 +0900

    feat: Add custom int array sample

---
+++ src/dataStructure/DataStructureTest.java
@@ -0,0 +1,22 @@
+package dataStructure;
+
+public class DataStructureTest {
+
+    public static void main(String[] args) {
+        MyArray arr = new MyArray(5);
+        System.out.println(arr.getSize());
+        arr.addElement(0);
+        arr.addElement(1);
+        arr.addElement(2);
+        arr.addElement(3);
+        System.out.println(arr);
+
+        arr.removeElement(1);
+        System.out.println(arr);
+
+        arr.insertElement(1,1);
+        System.out.println(arr);
+        arr.insertElement(1,5);
+        System.out.println(arr);
+    }
+}
---
+++ src/dataStructure/MyArray.java
@@ -0,0 +1,87 @@
+package dataStructure;
+
+public class MyArray {
+
+    int[] intArr;
+    int count;
+
+    public int ARRAY_SIZE;
+    public static final int ERROR_NUM = -999999999;
+
+    public MyArray() {
+        count = 0;
+        ARRAY_SIZE = 10;
+        intArr = new int[ARRAY_SIZE];
+    }
+
+    public MyArray(int size) {
+        count = 0;
+        ARRAY_SIZE = size;
+        intArr = new int[ARRAY_SIZE];
+    }
+
+    public void addElement(int num) {
+        if (count >= ARRAY_SIZE) {
+            System.out.println("Not enough memory");
+            return;
+        }
+        intArr[count++] = num;
+    }
+
+    public void insertElement(int position, int num) {
+        if (position < 0 || position > count) {
+            System.out.println("Invalid position");
+            return;
+        }
+        if (count >= ARRAY_SIZE) {
+            System.out.println("Not enough memory");
+            return;
+        }
+
+        for (int index = count - 1; index >= position; index--) {
+            intArr[index+1] = intArr[index];
+        }
+        intArr[position] = num;
+        count++;
+    }
+
+    public int removeElement(int position) {
+        int ret = ERROR_NUM;
+        if (isEmpty()) {
+            System.out.println("Array is empty");
+            return ret;
+        }
+        if (position < 0 || position > count - 1) {
+            System.out.println("Invalid position");
+            return ret;
+        }
+
+        ret = intArr[position];
+
+        for (int index = position; index < count - 1; index++) {
+            intArr[index] = intArr[index + 1];
+        }
+        count--;
+        return ret;
+    }
+
+    public int getSize() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        if (count == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int index = 0; index < count; index++) {
+            builder.append(index == 0 ? intArr[index] : ", " + intArr[index]);
+        }
+        return builder.toString();
+    }
+}
