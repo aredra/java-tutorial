524a0b5a74e416eba08f8e10bc9145a5f4f5d4bc
aredra <rnswn48@gmail.com>
2022-11-17 19:56:19 +0900

    Add spring-boot get method sample

---
+++ hello/src/main/java/com/example/hello/controller/ApiController.java
@@ -0,0 +1,52 @@
+package com.example.hello.controller;
+
+import com.example.hello.dto.UserRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+
+    @GetMapping(path = "/path-variable/{message}")
+    public String returnPathVariable(@PathVariable() String message) {
+        return "Hi " + message;
+    }
+
+    @GetMapping("/query")
+    public String returnQueryParameter(@RequestParam Map<String, String> param) {
+
+        StringBuilder sb = new StringBuilder();
+
+        param.forEach((key, value) -> {
+            System.out.println(key);
+            System.out.println(value);
+            System.out.println("\n");
+
+            sb.append(key).append(" : ").append(value).append("\n");
+        });
+
+        return sb.toString();
+    }
+
+    @GetMapping("/query-particle")
+    public String returnQueryParameter(@RequestParam String name,
+                                       @RequestParam int age,
+                                       @RequestParam String job) {
+
+
+        return name + ", " + age + ", " + job;
+    }
+
+    @GetMapping("/query-dto")
+    public String returnQueryParameter(UserRequest userRequest) {
+
+        return userRequest.toString();
+    }
+}
