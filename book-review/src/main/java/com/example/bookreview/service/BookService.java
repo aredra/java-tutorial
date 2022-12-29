ceafd2c3fa0c61e93791c6dbed78055827b771ab
aredra <rnswn48@gmail.com>
2022-12-29 19:42:30 +0900

    Add transaction sampe #1

--- book-review/src/main/java/com/example/bookreview/service/BookService.java
+++ book-review/src/main/java/com/example/bookreview/service/BookService.java
@@ -1,6 +1,9 @@
 package com.example.bookreview.service;
 
+import com.example.bookreview.domain.Author;
 import com.example.bookreview.domain.Book;
+import com.example.bookreview.repository.AuthorRepository;
+import com.example.bookreview.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,6 +15,10 @@ import javax.persistence.EntityManager;
 @RequiredArgsConstructor
 public class BookService {
 
+    private final BookRepository bookRepository;
+
+    private final AuthorRepository authorRepository;
+
     private final EntityManager entityManager;
 
     public void persistenceContext() {
@@ -19,19 +26,25 @@ public class BookService {
 
         // 비영속 - 자바 객체
         Book book = new Book();
-
         // 영속
         entityManager.persist(book);
-
         // 준영속(detached) - 영속에서 벗어남,
         // close, clear 로도 가능 작업 대기중인 작업이 없어질 수 도 있음
         entityManager.detach(book);
-
         // 준영속을 명시적으로 반영
         entityManager.merge(book);
-
         // 제거
         entityManager.remove(book);
-
     }
+
+    public void putBookAndAuthor() {
+        Book book = new Book();
+        book.setTitle("JPA tutorial");
+        bookRepository.save(book);
+
+        Author author = new Author();
+        author.setName("Dev1");
+        authorRepository.save(author);
+    }
+
 }
