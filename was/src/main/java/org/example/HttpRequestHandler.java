3928fe32a226e9b7e8a639d2f5615249a6080aa9
aredra <rnswn48@gmail.com>
2025-04-07 20:01:43 +0900

    feat: Add pages

--- was/src/main/java/org/example/HttpRequestHandler.java
+++ was/src/main/java/org/example/HttpRequestHandler.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.URLDecoder;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.example.utility.MyLogger.log;
@@ -44,11 +45,83 @@ public class HttpRequestHandler implements Runnable {
             log("HTTP request: " + requestString);
 
             log("HTTP response start");
-            sleep(3000);
+            if (requestString.startsWith("GET /site1")) {
+                site1(writer);
+            } else if (requestString.startsWith("GET /site2")) {
+                site2(writer);
+            } else if (requestString.startsWith("GET /search")) {
+                search(writer, requestString);
+            } else if (requestString.startsWith("GET / ")) {
+                home(writer);
+            } else {
+                notFound(writer);
+            }
             responseToClient(writer);
             log("HTTP response end");
 
         }
+    }
+
+
+    private void search(PrintWriter writer, String requestString) {
+        int startIndex = requestString.indexOf("q=");
+        int endIndex = requestString.indexOf(" ", startIndex + 2);
+        String searchString = requestString.substring(startIndex, endIndex);
+        String[] split = searchString.split("&");
+
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>Search</h1>");
+        writer.println("<ul>");
+        for (String query : split) {
+            String[] querySplit = query.split("=");
+            writer.println("<li>query: " + querySplit[1] + "</li>");
+            writer.println("<li>decode: " +  URLDecoder.decode(querySplit[1], UTF_8) + "</li>");
+        }
+        writer.println("</ul>");
+        writer.flush();
+    }
+
+
+    private void notFound(PrintWriter writer) {
+        writer.println("HTTP/1.1 404 Not Found");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>404 - Not Found</h1>");
+        writer.flush();
+    }
+
+
+    private void site2(PrintWriter writer) {
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>Site 2</h1>");
+        writer.flush();
+    }
+
+
+    private void site1(PrintWriter writer) {
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>Site 1</h1>");
+        writer.flush();
+    }
+
+
+    private void home(PrintWriter writer) {
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>Home</h1>");
+        writer.println("<ul>");
+        writer.println("<li><a href=\"/search?q=hello\">Search</a></li>");
+        writer.println("<li><a href=\"/site1\">Site 1</a></li>");
+        writer.println("<li><a href=\"/site2\">Site 2</a></li>");
+        writer.println("</ul>");
+        writer.flush();
     }
 
 
